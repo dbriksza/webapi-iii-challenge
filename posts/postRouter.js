a19a98b4f425254fb42028070867e08520d2ccc0
dbriksza <dbriksza@gmail.com>
2019-10-31 15:17:55 -0400

    forgetting to commit all week

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,63 @@
-const express = 'express';
+const router = require("express").Router();
+const postdb = require("./postDb");
 
-const router = express.Router();
-
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  postdb
+    .get()
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "unable to get posts" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  postdb
+    .getById(req.params.id)
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "unable to fins post with specified ID" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", (req, res) => {
+  postdb
+    .remove(req.params.id)
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "post was not deleted" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-
+router.put("/:id", (req, res) => {
+  postdb
+    .update(req.params.id, req.body)
+    .then(db => {
+      res.status(200).json(db);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "unable to edit post" });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+  postdb
+    .getById(req.params.id)
+    .then(next())
+    .catch(err => {
+      res.status(400).json({ message: "invalid post ID" });
+    });
+}
 
-};
+router.use("/:id", validatePostId);
 
 module.exports = router;
